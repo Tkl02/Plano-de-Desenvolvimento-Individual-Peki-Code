a07833f0c989676ae74db5a85616f1b374e3b5f4
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2024-06-11 15:29:42 -0300

    Atualizando Progresso
    * adicionando regra para ignorar "keys"
    * criando a interface teste do app
    * removendo o "test.dart" para evitar warnings

--- aplicativo/pokecatching/lib/main.dart
+++ aplicativo/pokecatching/lib/main.dart
@@ -7,9 +7,27 @@ void main() {
 class PokeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'hello world',
-      textDirection: TextDirection.rtl,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('POKE NATION'),
+        backgroundColor: const Color.fromARGB(255, 1, 141, 255),
+      ),
+      body: const Center(
+          child: Text(
+        'hello world',
+      )),
+      backgroundColor: const Color.fromARGB(249, 255, 0, 0),
     );
   }
 }
