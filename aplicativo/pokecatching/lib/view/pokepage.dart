62393b43c01930fdb7b8a2faab0e46b7752d31d0
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2024-06-25 10:29:24 -0300

    stl -> stf
    mudei a estrutura do codigo de stateless para stateful, para poder implementar os estados da aplicação.

--- aplicativo/pokecatching/lib/view/pokepage.dart
+++ aplicativo/pokecatching/lib/view/pokepage.dart
@@ -1,24 +1,62 @@
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
-class PokePage extends StatelessWidget {  @override
+class PokePage extends StatefulWidget {
+  @override
+  _PokePageState createState() => _PokePageState();
+}
+
+class _PokePageState extends State<PokePage> {
+  int _selectedValue = 0;
+
+  void _onImageTap(int value) {
+    setState(() {
+      _selectedValue = value;
+    });
+    print('Selected value: $_selectedValue');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 0, 0),
+      backgroundColor: Color.fromRGBO(255, 0, 0, 1),
       body: Center(
         child: Container(
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                    color: Color.fromARGB(255, 251, 255, 0),
+                    spreadRadius: (3),
+                    blurRadius: (0.1),
+                    offset: Offset(-1, 3))
+              ]),
           width: 345,
           height: 680,
-          color: Color.fromARGB(255, 255, 255, 255),
-          child: Column(children: [
-            Text('Rate cating of pokemon'),
-            //TODO CRIAR BOX DE INPUT 
-            Text('Select Pokeball'),
-            //TODO CRIAR SELEÇÃO DE POKE BOLA + FUNCIONALIDADE BOTÃO E ATRIBUIR VALORES.
-            Text('Select Status'),
-            //TODO CRIAR SELEÇÃO DE STATUS + FUNCIONALIDADE BOTÃO E ATRIBUIR VALORES.
-          ],
-          
+          child: Column(
+            children: [
+              Text('Rate cating of pokemon'),
+              //TODO
+
+              Text('Select Pokeball'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () => _onImageTap(1),
+                    child: Image.asset(
+                      'assets/bottons/pokeballred.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                  )
+                ],
+              ),
+              Text('Select Status'),
+              //TODO CRIAR SELEÇÃO DE STATUS + FUNCIONALIDADE BOTÃO E ATRIBUIR VALORES.
+            ],
           ),
         ),
       ),
